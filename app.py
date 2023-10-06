81a3680fd20d0eb35345a1b520cee7064ed884be
GitHamza0206 <43603227+GitHamza0206@users.noreply.github.com>
2023-10-06 18:11:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ def get_table_download_link(df, file_format, filename):
 def display_pdf(pdf_file):
     with open(pdf_file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    #pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
